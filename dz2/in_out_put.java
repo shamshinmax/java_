f3b6c5767cec3f53ee1451f6a1ed0c1cf4c52df4
shamshinmax <mksmsmsn@gmail.com>
2022-06-13 21:22:54 +0300

    edit

--- dz2/in_out_put.java
+++ dz2/in_out_put.java
@@ -1,18 +1,18 @@
-package java3;
+package dz2;
 
 import java.util.Scanner;
 
 
 public class in_out_put {
     
-    public static int get_num() {
+    int get_num() {
         Scanner scanner = new Scanner(System.in);
         int result = scanner.nextInt();
         scanner.close();
         return result;
         
     }
-    public static int triangle_num(int num){
+    int triangle_num(int num){
         int Tnum = (num+1)*num/2;
         return Tnum;
     }
